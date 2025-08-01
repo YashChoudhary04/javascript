8df15f0f7bc22f7c0583e65da19670447660fecc
YashChoudhary04 <yashvardhan754@gmail.com>
2025-08-01 05:23:08 +0000

    day2

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId=145226
+let accountEmail="yashvardhan754@gmail.com"
+var accountPassword="21546"
+accountcity="noida"
+//accountId=21546//you can't change const
+accountEmail="yash@54"
+accountPassword="56662"
+accountcity="mbd"
+console.table([accountId,accountEmail,accountPassword,accountPassword])//this will print the entire details in a table.
+console.log(accountId);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,5 @@
+"use strict"//treat all code in newer version
+let name="yash"//string
+let age=20//int
+let isLoggedin=false//boolen
+console.log(typeof age);
